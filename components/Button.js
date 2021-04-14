526aeca4d00dd03910d10c1b9b9010d81ff751c4
varinder-dev7701 <50487334+varinder-dev7701@users.noreply.github.com>
2021-04-14 12:51:27 +0530

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,16 @@
 import styles from './Button.module.css'
+import {useRouter} from 'next/router'
 
  export default function Button() {
+  const router = useRouter()
+
+  const handleClick = e => {
+    e.preventDefault()
+    router.push('/')
+  }
   return (
     <button
-      type="button"
+      type="button" onClick={handleClick}
       // Note how the "error" class is accessed as a property on the imported
       // `styles` object.
       className={styles.error}
